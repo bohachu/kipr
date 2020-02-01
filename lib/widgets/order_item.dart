cb5f03e2448418a0e6f435d45359509191c28a47
chialolee <carole1727@gmail.com>
2020-02-01 17:56:36 +0800

    001 done 1* 訂單的日期顯示格式改為 yyyy/mm/dd 要吻合臺灣習慣

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -30,7 +30,7 @@ class _OrderItemState extends State<OrderItem> {
             ListTile(
               title: Text('\$${widget.order.amount}'),
               subtitle: Text(
-                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+                DateFormat('yyyy/MM/dd hh:mm').format(widget.order.dateTime),
               ),
               trailing: IconButton(
                 icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
