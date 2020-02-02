51d8241e9f7a296603213aa363af47d78f1e2b40
Bowen Chiu <cbh@cameo.tw>
2020-02-02 19:22:47 +0800

    清理 flutter web upload 不需要的程式碼

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -3,12 +3,7 @@ import 'package:provider/provider.dart';
 import '../providers/product.dart';
 import '../providers/products.dart';
 import '../cameo/i18nKipr.dart';
-import 'dart:html' as html;
-import 'dart:typed_data';
 import 'dart:async';
-import 'dart:convert';
-import 'package:http_parser/http_parser.dart';
-import 'package:http/http.dart' as http;
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -23,10 +18,6 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
   final _form = GlobalKey<FormState>();
-  List<int> _selectedFile;
-  Uint8List _bytesData;
-
-  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
   var _editedProduct = Product(
     id: null,
@@ -142,48 +133,6 @@ class _EditProductScreenState extends State<EditProductScreen> {
     });
     Navigator.of(context).pop();
     // Navigator.of(context).pop();
-  }
-
-  startWebFilePicker() async {
-    html.InputElement uploadInput = html.FileUploadInputElement();
-    uploadInput.multiple = true;
-    uploadInput.draggable = true;
-    uploadInput.click();
-
-    uploadInput.onChange.listen((e) {
-      final files = uploadInput.files;
-      final file = files[0];
-      final reader = new html.FileReader();
-
-      reader.onLoadEnd.listen((e) {
-        _handleResult(reader.result);
-      });
-      reader.readAsDataUrl(file);
-    });
-  }
-
-  void _handleResult(Object result) {
-    setState(() {
-      _bytesData = Base64Decoder().convert(result.toString().split(",").last);
-      _selectedFile = _bytesData;
-    });
-  }
-
-  Future<String> makeRequest() async {
-    var url = Uri.parse(
-        "http://192.168.23.10/upload_api/web/app_dev.php/api/save-file/");
-    var request = new http.MultipartRequest("POST", url);
-    request.files.add(await http.MultipartFile.fromBytes('file', _selectedFile,
-        contentType: new MediaType('application', 'octet-stream'),
-        filename: "file_up"));
-
-    request.send().then((response) {
-      print("edit_product_screen.dart/makeRequest() send() 001");
-      print(response.statusCode);
-      if (response.statusCode == 200)
-        print(
-            "edit_product_screen.dart/makeRequest() statusCode ${response.statusCode}");
-    });
   }
 
   @override
